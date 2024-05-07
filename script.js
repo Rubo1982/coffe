2c2111c0024d1bdc0db0b5965412764555024287
Rubo1982 <babujyan.82@mail.ru>
2024-05-07 07:09:44 +0400

    ggg

---
+++ script.js
@@ -0,0 +1,113 @@
+function mak(){
+let entrutyun = prompt("duq @ntrel eq mak coffe ");
+if (entrutyun == "ayo"){
+    let gumar = +prompt("mutqagreq gumar@");
+    if (gumar >= 200){
+        gumar = gumar -200;
+        alert(gumar + " " + "dram" + " " + "vercreq der gumar@");
+    }
+    else{
+        alert ("anbavarar gumar")
+    }
+}
+
+}
+function latte(){
+let entrutyun = prompt("duq @ntrel eq latte coffe");
+if (entrutyun == "ayo"){
+    let gumar = +prompt("mutqagreq gumar@");
+    if (gumar >= 300){
+        gumar = gumar -300;
+        alert(gumar + " " + "dram" + " " + "vercreq der gumar@");
+    }
+    else{
+        alert ("anbavarar gumar")
+    }
+}
+
+}
+function karkade(){
+let entrutyun = prompt("duq @ntrel eq tea karkade");
+if (entrutyun == "ayo"){
+    let gumar = +prompt("mutqagreq gumar@");
+    if (gumar >= 150){
+        gumar = gumar -150;
+        alert(gumar + " " + "dram" + " " + "vercreq der gumar@");
+    }
+    else{
+        alert ("anbavarar gumar")
+    }
+}
+
+}
+function capuchino(){
+let entrutyun = prompt("duq @ntrel eq capuchino");
+if (entrutyun == "ayo"){
+    let gumar = +prompt("mutqagreq gumar@");
+    if (gumar >= 250){
+        gumar = gumar -250;
+        alert(gumar + " " + "dram" + " " + "vercreq der gumar@");
+    }
+    else{
+        alert ("anbavarar gumar")
+    }
+}
+}
+
+function maktea(){
+let entrutyun = prompt("duq @ntrel eq mak tea");
+if (entrutyun == "ayo"){
+    let gumar = +prompt("mutqagreq gumar@");
+    if (gumar >= 150){
+        gumar = gumar -150;
+        alert(gumar + " " + "dram" + " " + "vercreq der gumar@");
+    }
+    else{
+        alert ("anbavarar gumar")
+    }
+}
+}
+function nescaffe(){
+let entrutyun = prompt("duq @ntrel eq nescaffe coffe");
+if (entrutyun == "ayo"){
+    let gumar = +prompt("mutqagreq gumar@");
+    if (gumar >= 250){
+        gumar = gumar -250;
+        alert(gumar + " " + "dram" + " " + "vercreq der gumar@");
+    }
+    else{
+        alert ("anbavarar gumar")
+    }
+}
+
+}
+function sevsurj(){
+let entrutyun = prompt("entrir qo coffenduq @ntrel eq sev surj");
+if (entrutyun == "ayo"){
+    let gumar = +prompt("mutqagreq gumar@");
+    if (gumar >= 100){
+        gumar = gumar -100;
+        alert(gumar + " " + "dram" + " " + "vercreq der gumar@");
+    }
+    else{
+        alert ("anbavarar gumar")
+    }
+}
+
+}
+function americano(){
+let entrutyun = prompt("duq @ntrel eq americano ");
+if (entrutyun == "ayo"){
+    let gumar = +prompt("mutqagreq gumar@");
+    if (gumar >= 300){
+        gumar = gumar -300;
+        alert(gumar + " " + "dram" + " " + "vercreq der gumar@");
+    }
+    else{
+        alert ("anbavarar gumar")
+    }
+}
+
+}
+
+
